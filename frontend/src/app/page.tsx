49e55534946a2c4e1fb051ad9470a51d19b5ed5f
stasel <2033301+stasel@users.noreply.github.com>
2026-08-13 19:57:46 +0200

    Minor change on frontend to trigger build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>HYF Final Project</h1>
+      <h1>HYF Final Project!</h1>
       <Link href="/users">View users</Link>
     </div>
   );
